0e94cbb1fc57935b29fe824fdd040a4054af89ca
Ubin108 <ybin0108@gmail.com>
2024-12-27 04:18:01 +0900

    add teampage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
+import TeamPage from './TeamPage';
 import './App.css';
 
 const App = () => {
     const [currentIndex, setCurrentIndex] = useState(1); // Start at the first image in the extended array
     const [isTransitioning, setIsTransitioning] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
 
     const images = [
-        { src: process.env.PUBLIC_URL + '/DiffuseHigh.jpg', alt: 'DiffuseHigh' },
-        { src: process.env.PUBLIC_URL + '/CodecNerf.jpg', alt: 'CodecNerf' },
-        { src: process.env.PUBLIC_URL + '/SequenceMatters.jpg', alt: 'SequenceMatters' },
-        { src: process.env.PUBLIC_URL + '/PEVC.jpg', alt: 'PEVC' },
+        { src: process.env.PUBLIC_URL + '/home/DiffuseHigh.jpg', alt: 'DiffuseHigh' },
+        { src: process.env.PUBLIC_URL + '/home/CodecNerf.jpg', alt: 'CodecNerf' },
+        { src: process.env.PUBLIC_URL + '/home/SequenceMatters.jpg', alt: 'SequenceMatters' },
+        { src: process.env.PUBLIC_URL + '/home/PEVC.jpg', alt: 'PEVC' },
     ];
 
     const extendedImages = [images[images.length - 1], ...images, images[0]]; // Add last and first image for smooth looping
+
+    const newsItems = [
+        "[2024.12] Three papers, including DiffuseHigh and CodecNeRF, got accepted to AAAI 2025!",
+        "[2024.11] DiffuseHigh received the best paper award at JKAIA 2024!",
+        "[2024.10] Invited talk at SKKU AI Colloqium 2024 Recorded talk (Korean)",
+        "[2024.09] PEFT Video Compression work was selected as an oral presentation in ACCV 2024!",
+        "[2024.09] Freq-Mip-AA is considered as a best paper candidate in ICIP 2024!",
+        "[2024.07] Two papers accepted to ECCV 2024!",
+        "[2024.05] Our lab received the outstanding young scientist grant from National Research Foundation!",
+        "[2024.05] Our lab (with Prof Inki Kim) won the grant from Samsung Science & Technology Foundation!",
+        "[2024.04] Two CVPR 2024 papers selected as highlight!",
+        "[2024.02] Two papers accepted to CVPR 2024!",
+        "[2024.01] CAM was accepted to ICLR 2024 as a spotlight!",
+        "[2023.11] Invited talk at GIST Colloquium",
+        "[2023.09] SPINN was accepted to NeurIPS 2023 as a spotlight!",
+        "[2023.09] Two papers accepted to NeurIPS 2023!",
+        "[2023.05] Invited talk at UNIST AI graduate seminar",
+        "[2023.02] Two papers accepted to CVPR 2023!",
+        "[2023.02] Invited talk about SPINN at CRUNCH Seminars at Brown University [Recorded talk]",
+        "[2022.11] Invited talk at 2022 KSIAM Annual Meeting",
+        "[2022.11] PIXEL was accepted to AAAI 2023",
+        "[2022.09] NRFF was accepted to ACCV 2022",
+        "[2022.08] PIXEL was selected as a spotlight in DLDE-II (NeurIPS 2022 Workshop)",
+        "[2022.08] SPINN received the best paper award in CKAIA 2022",
+        "[2022.07] Streamable Neural Fields was accepted to ECCV 2022",
+        "[2022.05] Our lab received the Basic Research Lab (BRL) grant from the NRF",
+        "[2022.03] Invited talk at Samsung Research",
+        "[2021.09] Honor to be selected as a EIRIC rising star",
+        "[2021.06] VSC lab was founded",
+        "[2021.05] Invited talk at AI Frontiers Summit 2021",
+    ];
+
+    const itemsPerPage = 10;
+    const totalPages = Math.ceil(newsItems.length / itemsPerPage);
 
     const handleNext = () => {
         if (isTransitioning) return;
@@ -49,82 +86,94 @@ const App = () => {
         return () => clearInterval(interval);
     }, []);
 
+    const currentNewsItems = newsItems.slice(
+        (currentPage - 1) * itemsPerPage,
+        currentPage * itemsPerPage
+    );
+
     return (
-        <div className="App">
-            <nav className="navbar">
-                <div className="title">Visual and Scientific Computing Lab</div>
-                <div className="menu">
-                    <a href="#">Home</a>
-                    <a href="https://silverbottlep.github.io/pair.html">Team</a>
-                    <a href="https://silverbottlep.github.io/publication.html">Publication</a>
-                    <a href="https://silverbottlep.github.io/pic.html">Gallery</a>
-                    <a href="https://silverbottlep.github.io/index.html">Contact</a>
-                </div>
-            </nav>
-
-            <main className="main">
-                <div className="carousel">
-                    <button className="carousel-button prev" onClick={handlePrev}>
-                        <span className="arrow">&#9664;</span>
-                    </button>
-                    <div
-                        className="carousel-images"
-                        style={{
-                            transform: `translateX(-${currentIndex * 100}%)`,
-                            transition: isTransitioning ? 'transform 0.5s ease-in-out' : 'none',
-                        }}
-                    >
-                        {extendedImages.map((image, index) => (
-                            <img
-                                key={index}
-                                src={image.src}
-                                alt={image.alt}
-                                className="carousel-image"
-                            />
-                        ))}
+        <Router>
+            <div className="App">
+                <nav className="navbar">
+                    <div className="title">Visual and Scientific Computing Lab</div>
+                    <div className="menu">
+                        <Link to="/">Home</Link>
+                        <Link to="/team">Team</Link>
+                        <Link to="/publication">Publication</Link>
+                        <Link to="/gallery">Gallery</Link>
+                        <Link to="/contact">Contact</Link>
                     </div>
-                    <button className="carousel-button next" onClick={handleNext}>
-                        <span className="arrow">&#9654;</span>
-                    </button>
-                    <div className="carousel-indicators">
-                        {images.map((_, index) => (
-                            <span
-                                key={index}
-                                className={`indicator ${index + 1 === currentIndex ? 'active' : ''}`}
-                                onClick={() => {
-                                    if (index + 1 !== currentIndex) {
-                                        setIsTransitioning(true);
-                                        setCurrentIndex(index + 1);
-                                    }
-                                }}
-                            ></span>
-                        ))}
-                    </div>
-                </div>
-            </main>
+                </nav>
 
-            <section className="news">
-                <h2>News</h2>
-                <ul className="news-list">
-                    <li>[2024.12] Three papers, including DiffuseHigh and CodecNeRF, got accepted to AAAI 2025!</li>
-                    <li>[2024.11] DiffuseHigh received the best paper award at JKAIA 2024!</li>
-                    <li>[2024.10] Invited talk at SKKU AI Colloqium 2024 Recorded talk (Korean)</li>
-                    <li>[2024.09] PEFT Video Compression work was selected as an oral presentation in ACCV 2024!</li>
-                    <li>[2024.09] Freq-Mip-AA is considered as a best paper candidate in ICIP 2024!</li>
-                    <li>[2024.07] Two papers accepted to ECCV 2024!</li>
-                    <li>[2024.05] Our lab received the outstanding young scientist grant from National Research Foundation!</li>
-                    <li>[2024.05] Our lab (with Prof Inki Kim) won the grant from Samsung Science & Technology Foundation!</li>
-                    <li>[2024.04] Two CVPR 2024 papers selected as highlight!</li>
-                    <li>[2024.02] Two papers accepted to CVPR 2024!</li>
-                    <li>[2024.01] CAM was accepted to ICLR 2024 as a spotlight!</li>
-                </ul>
-            </section>
-            <footer className="footer">
-                <p>Sungkyunkwan University</p>
-                <p>(16419) 2066, SEOBU-RO, JANGAN-GU, SUWON-SI, GYEONGGI-DO, KOREA</p>
-                <p>Contact: epark@skku.edu</p>
-            </footer>
-        </div>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <>
+                                <main className="main">
+                                    <div className="carousel">
+                                        <button className="carousel-button prev" onClick={handlePrev}>
+                                            <span className="arrow">&#9664;</span>
+                                        </button>
+                                        <div
+                                            className="carousel-images"
+                                            style={{
+                                                transform: `translateX(-${currentIndex * 100}%)`,
+                                                transition: isTransitioning
+                                                    ? 'transform 0.5s ease-in-out'
+                                                    : 'none',
+                                            }}
+                                        >
+                                            {extendedImages.map((image, index) => (
+                                                <img
+                                                    key={index}
+                                                    src={image.src}
+                                                    alt={image.alt}
+                                                    className="carousel-image"
+                                                />
+                                            ))}
+                                        </div>
+                                        <button className="carousel-button next" onClick={handleNext}>
+                                            <span className="arrow">&#9654;</span>
+                                        </button>
+                                    </div>
+                                </main>
+
+                                <section className="news">
+                                    <h2>News</h2>
+                                    <ul className="news-list">
+                                        {currentNewsItems.map((item, index) => (
+                                            <li key={index}>{item}</li>
+                                        ))}
+                                    </ul>
+                                    <div className="pagination">
+                                        {Array.from({ length: totalPages }, (_, index) => (
+                                            <button
+                                                key={index}
+                                                className={`pagination-button ${
+                                                    currentPage === index + 1 ? 'active' : ''
+                                                }`}
+                                                onClick={() => setCurrentPage(index + 1)}
+                                            >
+                                                {index + 1}
+                                            </button>
+                                        ))}
+                                    </div>
+                                </section>
+                            </>
+                        }
+                    />
+                    <Route path="/team" element={<TeamPage />} />
+                    <Route path="*" element={<Navigate to="/" />} /> {}
+                </Routes>
+
+                <footer className="footer">
+                    <p>Sungkyunkwan University</p>
+                    <p>(16419) 2066, SEOBU-RO, JANGAN-GU, SUWON-SI, GYEONGGI-DO, KOREA</p>
+                    <p>Contact: epark@skku.edu</p>
+                </footer>
+            </div>
+        </Router>
     );
 };
 
---
+++ src/TeamPage.js
@@ -0,0 +1,271 @@
+import React from 'react';
+import './TeamPage.css';
+
+const TeamPage = () => {
+    const graduateStudents = [
+        { 
+            name: 'Joo Chan Lee', 
+            img: process.env.PUBLIC_URL + '/team/temp.jpg', 
+            email: 'joolee@example.com', 
+            scholar: 'https://scholar.google.com/citations?user=WKCHKk8AAAAJ&hl=en',
+            github: 'https://github.com/maincold2' 
+        },
+        { 
+            name: 'Younggeun Lee', 
+            img: process.env.PUBLIC_URL + '/team/temp.jpg', 
+            email: 'younglee@example.com', 
+            scholar: 'https://scholar.google.com/citations?hl=en&user=E4iEBFsAAAAJ',
+            github: 'https://github.com/Younggeun-L' 
+        },
+        { 
+            name: 'Seungtae Nam', 
+            img: process.env.PUBLIC_URL + '/team/temp.jpg', 
+            email: 'seungtae@example.com', 
+            scholar: 'https://scholar.google.com/citations?user=8NKPmmwCmrAC&hl=en',
+            github: 'https://github.com/stnamjef'
+        },
+        { 
+            name: 'Xiangyu Sun', 
+            img: process.env.PUBLIC_URL + '/team/temp.jpg',
+            email: 'seungtae@example.com', 
+            scholar: 'https://scholar.google.com/citations?user=VLzxTrAAAAAJ&hl=en',
+            github: 'https://github.com/Xiangyu1Sun'  
+        },
+        { 
+            name: 'Namgyu Kang', 
+            img: process.env.PUBLIC_URL + '/team/temp.jpg', 
+            email: 'seungtae@example.com', 
+            scholar: 'https://scholar.google.com/citations?hl=en&user=yb9y9k8AAAAJ',
+            github: 'https://github.com/NamGyuKang' 
+        },
+        { 
+            name: 'Byeonghyeon Lee', 
+            img: process.env.PUBLIC_URL + '/team/temp.jpg', 
+            email: 'seungtae@example.com', 
+            scholar: 'https://scholar.google.com/citations?user=_PhPccYAAAAJ&hl=en',
+            github: 'https://github.com/benhenryL' 
+        },
+        { 
+            name: 'Junyu Zhang', 
+            img: process.env.PUBLIC_URL + '/team/temp.jpg',
+            email: 'seungtae@example.com', 
+            scholar: 'https://scholar.google.com/citations?hl=en&user=3jrCsVoAAAAJ',
+            github: ''  
+        },
+        { 
+            name: 'Younghyun Kim', 
+            img: process.env.PUBLIC_URL + '/team/temp.jpg',
+            email: 'seungtae@example.com', 
+            scholar: 'https://scholar.google.com/citations?hl=en&user=4M-6X7MAAAAJ',
+            github: 'https://github.com/yhyun225'  
+        },
+        { 
+            name: 'Dohyun Kim', 
+            img: process.env.PUBLIC_URL + '/team/temp.jpg',
+            email: 'seungtae@example.com', 
+            scholar: '',
+            github: 'https://github.com/unhyop'  
+        },
+        { 
+            name: 'Geunmin Hwang', 
+            img: process.env.PUBLIC_URL + '/team/temp.jpg',
+            email: 'seungtae@example.com', 
+            scholar: '',
+            github: 'https://github.com/GeunminHwang'  
+        },
+        { 
+            name: 'Seungjun Oh', 
+            img: process.env.PUBLIC_URL + '/team/temp.jpg',
+            email: 'seungtae@example.com', 
+            scholar: 'https://scholar.google.com/citations?hl=en&user=HMnjBk0AAAAJ',
+            github: 'https://github.com/ohsngjun'  
+        },
+        { 
+            name: 'Gyeongjin Kang', 
+            img: process.env.PUBLIC_URL + '/team/temp.jpg', 
+            email: 'seungtae@example.com', 
+            scholar: 'https://scholar.google.com/citations?user=iyQ16vIAAAAJ&hl=en',
+            github: 'https://github.com/Gynjn' 
+        },
+        { 
+            name: 'Dongheok Park', 
+            img: process.env.PUBLIC_URL + '/team/temp.jpg',
+            email: 'seungtae@example.com', 
+            scholar: '',
+            github: 'https://github.com/DHPark98'  
+        },
+        { 
+            name: 'Hyun-kyu Ko', 
+            img: process.env.PUBLIC_URL + '/team/temp.jpg',
+            email: 'seungtae@example.com', 
+            scholar: '',
+            github: 'https://github.com/Ko-Lani'  
+        },
+        { 
+            name: 'Jisang Yoo', 
+            img: process.env.PUBLIC_URL + '/team/temp.jpg',
+            email: 'seungtae@example.com', 
+            scholar: '',
+            github: 'https://github.com/Yoo-jisang'  
+        },
+        { 
+            name: 'Hye Jin Jeon', 
+            img: process.env.PUBLIC_URL + '/team/temp2.jpg',
+            email: 'seungtae@example.com', 
+            scholar: '',
+            github: 'https://github.com/jeonhhyejin'  
+        },
+        { 
+            name: 'Youbin Kim', 
+            img: process.env.PUBLIC_URL + '/team/temp.jpg',
+            email: 'ybin108@skku.edu', 
+            scholar: 'https://scholar.google.com/citations?hl=en&user=RUiA8-wAAAAJ',
+            github: 'https://github.com/Ubin108'  
+        },
+        { 
+            name: 'Ji Hyeon Park', 
+            img: process.env.PUBLIC_URL + '/team/temp2.jpg',
+            email: 'seungtae@example.com', 
+            scholar: '',
+            github: 'https://github.com/Kanadae'  
+        },
+        { 
+            name: 'Seungryong Lee', 
+            img: process.env.PUBLIC_URL + '/team/temp.jpg',
+            email: 'seungtae@example.com', 
+            scholar: '',
+            github: 'https://github.com/lms20031'  
+        },
+        { 
+            name: 'Eunsoo Lee', 
+            img: process.env.PUBLIC_URL + '/team/temp2.jpg',
+            email: 'seungtae@example.com', 
+            scholar: '',
+            github: 'https://github.com/esleeo'  
+        },
+    ];
+
+    const undergraduateStudents = [
+        { name: 'Jinho Park', img: process.env.PUBLIC_URL + '/team/temp.jpg' },
+        { name: 'Hyunsu Kim', img: process.env.PUBLIC_URL + '/team/temp2.jpg' },
+        { name: 'Woojeong Baek', img: process.env.PUBLIC_URL + '/team/temp2.jpg' },
+        { name: 'Seungkwon Yang', img: process.env.PUBLIC_URL + '/team/temp.jpg' },
+        { name: 'Minseo Lee', img: process.env.PUBLIC_URL + '/team/temp2.jpg' },
+    ];
+
+    const alumni = [
+        'Daniel Rho (MS, 2020.09 - 2022.08), co-advised by Jong Hwan Ko, KT → PhD student at UNC Chapel Hill',
+        'Hanbit Yoon (MS, 2021.09 - 2023.08), co-advised by Joon Hee Choi, Samsung Electronics',
+        'Sanghyeon Kim (MS, 2021.09 - 2023.08), LG CNS',
+        'Junwoo Cho (MS, 2021.09 - 2023.08), Lunit',
+        'Usman Ali (Research Professor, 2022.09 - 2024.02), assistant professor at Sejong University',
+        'Seungtae Nam (MS, 2022.03 - 2024.02), PhD student at VSC Lab, SKKU',
+        'Hyunmo Yang (MS, 2022.03 - 2024.02)',
+        'Namgyu Kang (MS, 2022.03 - 2024.08), PhD student at VSC Lab, SKKU',
+        'Byeonghyeon Lee (MS, 2022.09 - 2024.08), PhD student at VSC Lab, SKKU',
+        'Howoong Lee (MS, 2022.09 - 2024.08), industrial scholars program, Hanwha Vision',
+        'Youngin Park (MS, 2023.03 - 2024.08), part time industrial scholars program, Samsung Electronics]'];
+
+    return (
+        <div className="team-page">
+            <section>
+                <h2>Principal Investigator</h2>
+                <div className="profile">
+                    <div className="member">
+                        <img
+                            src={process.env.PUBLIC_URL + '/team/epark.jpg'}
+                            alt="Prof. Eunbyung Park"
+                            className="profile-img"
+                        />
+                        
+                        <a href="https://silverbottlep.github.io/index.html">Eunbyung Park</a>
+                        <div className='bio'>
+                            <a href="mailto:epark@skku.edu">
+                                <i className="fa fa-envelope-o" style={{ fontSize: '24px', color: 'black' }}></i>
+                            </a>
+                            <a href="https://silverbottlep.github.io/index.html">
+                                <i className="fa fa-home" style={{ fontSize: '24px', color: 'black' }}></i>
+                            </a>
+                            <a href="https://scholar.google.com/citations?user=iPyuJmQAAAAJ&hl=en">
+                                <i className="ai ai-google-scholar" style={{ fontSize: '24px', color: 'black' }}></i>
+                            </a>
+                            <a href="https://www.linkedin.com/in/eunbyung-park-286384b4/">
+                                <i className="fa fa-linkedin" style={{ fontSize: '24px', color: 'black' }}></i>
+                            </a>
+                            <a href="https://twitter.com/silverbottlep">
+                                <i className="fa fa-twitter" style={{ fontSize: '24px', color: 'black' }}></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="details">
+                        <p className="prof-intro">
+                            <strong>Education</strong>
+                            <ul>
+                                <li>Ph.D., Computer Science at University of North Carolina at Chapel Hill, 2014.09 - 2019.12</li>
+                                <li>M.S., Computer Science at Seoul National University, 2009.03 - 2011.02</li>
+                                <li>B.S., Computer Science at Kyung Hee University, 2002.03 - 2009.02</li>
+                            </ul>
+                            <strong>Work Experience</strong>
+                            <ul>
+                                <li>Assistant Professor, <a href="https://ice.skku.edu/eng_ice/programs/departments/elec_intro.do" target="_blank">SKKU</a>, 2021.02 - Present</li>
+                                <li>Applied Scientist, <a href="https://msturing.org" target="_blank">Microsoft Project Turing</a>, 2020.09 - 2021.02</li>
+                                <li>Research Scientist, <a href="http://nuro.ai/" target="_blank">Nuro</a>, 2019.06 - 2020.08</li>
+                                <li>Research Intern, <a href="https://deepmind.com/" target="_blank">Google DeepMind</a>, 2018.06 - 2018.10 </li>
+                                <li>Research Intern, <a href="https://www.microsoft.com/en-us/research/" target="_blank">Microsoft Research</a>, 2017.06 - 2017.08</li>
+                                <li>Research Intern, <a href="http://www.adobe.com/technology.html" target="_blank">Adobe Research</a>, 2016.06 - 2016.08</li>
+                                <li>Research Intern, <a href="https://www.hp.com/us-en/hp-labs.html"  target="_blank">HP Labs</a>, 2015.06 - 2015.08</li>
+                            </ul>
+                        </p>
+                        
+                    </div>
+                </div>
+            </section>
+
+
+            <section>
+                <h2>Graduate Students</h2>
+                <div className="students">
+                    {graduateStudents.map((student, index) => (
+                        <div key={index} className="member">
+                            <img src={student.img} alt={student.name} className="profile-img" />
+                            <p>{student.name}</p>
+                            <div className="student-links">
+                                {student.email && <a href={`mailto:${student.email}`}><i className="fa fa-envelope-o" style={{ fontSize: '24px', color: 'black' }}></i></a>}
+                                {student.scholar && <a href={student.scholar}><i className="ai ai-google-scholar" style={{ fontSize: '24px', color: 'black' }}></i></a>}
+                                {student.github && <a href={student.github}><i className="fa fa-github" style={{ fontSize: '24px', color: 'black' }}></i></a>}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </section>
+
+            <section>
+                <h2>Undergraduate Students</h2>
+                <div className="students">
+                    {undergraduateStudents.map((student, index) => (
+                        <div key={index} className="member">
+                            <img src={student.img} alt={student.name} className="profile-img" />
+                            <p>{student.name}</p>
+                            <div className="student-links">
+                                {student.email && <a href={`mailto:${student.email}`}><i className="fa fa-envelope-o" style={{ fontSize: '24px', color: 'black' }}></i></a>}
+                                {student.scholar && <a href={student.scholar}><i className="ai ai-google-scholar" style={{ fontSize: '24px', color: 'black' }}></i></a>}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </section>
+
+
+            <section>
+                <h2>Alumni</h2>
+                <ul className="alumni-list">
+                    {alumni.map((alumnus, index) => (
+                        <li key={index}>{alumnus}</li>
+                    ))}
+                </ul>
+            </section>
+        </div>
+    );
+};
+
+export default TeamPage;
